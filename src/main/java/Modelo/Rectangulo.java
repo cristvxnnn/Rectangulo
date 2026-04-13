b02941e792fcf98ab2b8f8804186f2d013732b4a
cristvxnnnn <cristian.torres_m@uao.edu.co>
2026-04-12 22:03:34 -0500

    En la clase hija Rectangulo se implemento el  cálculo de área

--- src/main/java/Modelo/Rectangulo.java
+++ src/main/java/Modelo/Rectangulo.java
@@ -8,9 +8,22 @@ package Modelo;
  *
  * @author crstc
  */
-public class Rectangulo {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Rectangulo extends Figura {
+    private Punto p2;
+    public Rectangulo(Punto p1, Punto p2){
+        super(p1);
+        this.p2=p2;
     }
+
+    @Override
+    public double calcularArea() {
+        double base = p2.getX()-posicion.getX();
+        double altura= p2.getY()-posicion.getX();
+        if(base<0)base=base*-1;
+        if(altura<0)altura=altura*-1;
+        return base * altura;
+    }
+    
+    
+
 }
